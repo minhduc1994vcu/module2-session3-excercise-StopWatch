623e57382ea6d5b0f14e2356475cbbae4f670f5b
duc.phamminh2 <duc.phamminh2@vndirect.com.vn>
2022-05-24 16:55:40 +0700

    udpate

--- src/main/java/StopWatch.java
+++ src/main/java/StopWatch.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class StopWatch {
     public static void main(String[] args) {
+        System.out.println("demo");
         StopWatch stopWatch = new StopWatch();
         Scanner scanner = new Scanner(System.in);
         int choise;
